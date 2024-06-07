3fe5bfbd301ad67a80e4ee446dd0eeff386ab469
mapoirie <mapoirie@mapoirie.42.fr>
2024-06-07 18:00:22 +0200

    7.07 18h

--- data/srcs/requirements/mariadb/tools/dbscript.sh
+++ data/srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,15 +1,19 @@
+# script qui sert a creer une database et un utilisateur associe, il sera exec par le Dockerfile
 #!/bin/bash
 #set -eux
 
+# demarre MySQL
 service mariadb start;
 
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
+# se log a MariaDB en root et creer la database et l'utilisateur
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
+# verifie si le serveur MariaDB est en cours d'exec et accessible
 mysqladmin ping -u root -p${SQL_ROOT_PASSWORD} || exit 1
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 exec mysqld_safe
